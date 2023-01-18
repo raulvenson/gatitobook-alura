82eefd32453d3bb5099a728631eeb733ffd65669
raul-venson-martinello <informatica16@martinello.com.br>
2023-01-18 06:20:39 -0400

    Implementing new animal

--- gatitobook/src/app/animais/animais.module.ts
+++ gatitobook/src/app/animais/animais.module.ts
@@ -12,6 +12,7 @@ import { ListaAnimaisComponent } from './lista-animais/lista-animais.component';
 import { AnimalComponent } from './animal/animal.component';
 import { DetalheAnimalComponent } from './detalhe-animal/detalhe-animal.component';
 import { ComentariosComponent } from './detalhe-animal/comentarios/comentarios.component';
+import { NovoAnimalComponent } from './novo-animal/novo-animal.component';
 
 @NgModule({
   declarations: [
@@ -20,6 +21,7 @@ import { ComentariosComponent } from './detalhe-animal/comentarios/comentarios.c
     GradeFotosAnimaisComponent,
     DetalheAnimalComponent,
     ComentariosComponent,
+    NovoAnimalComponent,
   ],
   imports: [CommonModule, AnimaisRoutingModule, CartaoModule, SharedModule],
 })
---
+++ gatitobook/src/app/animais/novo-animal/novo-animal.component.ts
@@ -0,0 +1,23 @@
+import { FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-novo-animal',
+  templateUrl: './novo-animal.component.html',
+  styleUrls: ['./novo-animal.component.css'],
+})
+export class NovoAnimalComponent implements OnInit {
+  formularioAnimal!: FormGroup;
+  file!: File;
+  preview!: string;
+  percentualConcluido = 0;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  upload() {}
+  gravarArquivo(arquivo: any){
+
+  }
+}
